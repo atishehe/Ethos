85c3ea7e14e0115c9891b77dfffa0987a318f130
atishehe <125atishprak@gmail.com>
2025-08-28 22:35:25 +0530

    Update server.js

--- exam-platform-backend-main/src/server.js
+++ exam-platform-backend-main/src/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5001;
 // Middleware
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://ethos-l2zhyxjcb-125atishprak-1360s-projects.vercel.app"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://ethos-l2zhyxjcb-125atishprak-1360s-projects.vercel.app","https://theintegralcup-seven.vercel.app/"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization", "x-requested-with"],
     credentials: true,
